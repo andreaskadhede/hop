3f2ae50231ef23b6b90bead20d6aff4221dbc080
Andrea Skadhede <andreaskadhede@icloud.com>
2025-05-01 18:28:34 +0200

    checkbox styling updated

--- src/components/small/Checkbox/Checkbox.tsx
+++ src/components/small/Checkbox/Checkbox.tsx
@@ -6,24 +6,22 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import * as RadixCheckbox from "@radix-ui/react-checkbox";
 
 const Checkbox = forwardRef<HTMLButtonElement, RadixCheckbox.CheckboxProps>(
-   () => {
-      return (
-         <>
-            <RadixCheckbox.Root
-               className={classNames(styles.checkbox)}
-               defaultChecked
-               id="c1">
-               <RadixCheckbox.Indicator
-                  className={classNames(styles.checkboxIndicator)}>
-                  <FontAwesomeIcon
-                     className={classNames(styles.checkIcon)}
-                     icon={faCheck}
-                  />
-               </RadixCheckbox.Indicator>
-            </RadixCheckbox.Root>
-         </>
-      );
-   }
+  () => {
+    return (
+      <>
+        <RadixCheckbox.Root className={classNames(styles.checkbox)} id="c1">
+          <RadixCheckbox.Indicator
+            className={classNames(styles.checkboxIndicator)}
+          >
+            <FontAwesomeIcon
+              className={classNames(styles.checkIcon)}
+              icon={faCheck}
+            />
+          </RadixCheckbox.Indicator>
+        </RadixCheckbox.Root>
+      </>
+    );
+  }
 );
 
 export { Checkbox };
