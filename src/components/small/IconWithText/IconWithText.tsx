ed44852444af3a509a5a4c4fa96f0808c0ed8d87
Andrea Skadhede <andreaskadhede@icloud.com>
2025-05-01 14:44:07 +0200

    IconWithText styling updated

--- src/components/small/IconWithText/IconWithText.tsx
+++ src/components/small/IconWithText/IconWithText.tsx
@@ -4,46 +4,45 @@ import classNames from "classnames";
 import { Icon } from "../Icon/Icon";
 
 export type IconWithTextProps = {
-   type:
-      | "Expand"
-      | "Secure"
-      | "Settings"
-      | "ThinkingBobles"
-      | "Flow"
-      | "Statistics"
-      | "Learning"
-      | "Devices"
-      | "Building"
-      | "Cloud"
-      | "Pencil"
-      | "GraduationCap"
-      | "Support"
-      | "Handshake"
-      | "Gears"
-      | "CheckBobles"
-      | "CheckBoble"
-      | "StopWatch"
-      | "Checklist"
-      | "FixToTheRight";
-   children: React.ReactNode;
-   horizontal?: boolean;
+  type:
+    | "Expand"
+    | "Secure"
+    | "Settings"
+    | "ThinkingBobles"
+    | "Flow"
+    | "Statistics"
+    | "Learning"
+    | "Devices"
+    | "Building"
+    | "Cloud"
+    | "Pencil"
+    | "GraduationCap"
+    | "Support"
+    | "Handshake"
+    | "Gears"
+    | "CheckBobles"
+    | "CheckBoble"
+    | "StopWatch"
+    | "Checklist"
+    | "FixToTheRight";
+  children: React.ReactNode;
+  horizontal?: boolean;
 };
 
 const IconWithText = forwardRef<HTMLDivElement, IconWithTextProps>(
-   ({ type, children, horizontal }, ref) => {
-      return (
-         <div
-            ref={ref}
-            className={classNames(styles.iconWithText, {
-               [styles.horizontal]: horizontal,
-            })}>
-            <Icon type={type} width={horizontal ? 6.25 : 10}></Icon>
-            <p style={{ textAlign: horizontal ? "left" : "right" }}>
-               {children}
-            </p>
-         </div>
-      );
-   }
+  ({ type, children, horizontal }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={classNames(styles.iconWithText, {
+          [styles.horizontal]: horizontal,
+        })}
+      >
+        <Icon type={type} width={horizontal ? 4.375 : 9.375}></Icon>
+        <p style={{ textAlign: horizontal ? "left" : "center" }}>{children}</p>
+      </div>
+    );
+  }
 );
 
 export { IconWithText };
