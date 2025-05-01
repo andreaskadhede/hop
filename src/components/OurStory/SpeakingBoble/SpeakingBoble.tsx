46ff69828a94203c67335690b327e3c3aa656610
Andrea Skadhede <andreaskadhede@icloud.com>
2025-05-01 18:03:42 +0200

    styling and setup for bubbles added

--- src/components/OurStory/SpeakingBoble/SpeakingBoble.tsx
+++ src/components/OurStory/SpeakingBoble/SpeakingBoble.tsx
@@ -1,40 +1,51 @@
 import { forwardRef } from "react";
 import styles from "./SpeakingBoble.module.css";
 import classNames from "classnames";
-import { Header1 } from "../Header1/Header1";
-import { Header2 } from "../Header2/Header2";
+import { Header1 } from "../../small/Header1/Header1";
+import { Header2 } from "../../small/Header2/Header2";
 import taleBoble from "../../../assets/taleboble.svg";
 
 type SpeakingBobleProps = {
-   headerContent: string;
-   text: string;
-   year: number;
-   direction?: "up" | "down";
+  headerContent: string;
+  text: string;
+  year: number;
+  direction?: "up" | "down";
 };
 
 const SpeakingBoble = forwardRef<HTMLDivElement, SpeakingBobleProps>(
-   ({ headerContent, text, year, direction }) => {
-      return (
-         <div className={classNames(styles.event)}>
-            <div className={classNames(styles.boble)}>
-               <img
-                  src={taleBoble}
-                  alt=""
-                  className={classNames(
-                     styles.image,
-                     { [styles.up]: direction === "up" },
-                     { [styles.down]: direction === "down" }
-                  )}
-               />
-               <div className={classNames(styles.text)}>
-                  <Header2 content={headerContent} />
-                  <p>{text}</p>
-               </div>
-            </div>
-            <Header1 content={year?.toString()} />
-         </div>
-      );
-   }
+  ({ headerContent, text, year, direction }) => {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: direction === "up" ? "column-reverse" : "column",
+        }}
+      >
+        <div className={classNames(styles.boble)}>
+          <img
+            src={taleBoble}
+            alt=""
+            className={classNames(
+              styles.image,
+              { [styles.up]: direction === "up" },
+              { [styles.down]: direction === "down" }
+            )}
+          />
+          <div
+            className={classNames(
+              styles.text,
+              { [styles.up]: direction === "up" },
+              { [styles.down]: direction === "down" }
+            )}
+          >
+            <Header2 content={headerContent} />
+            <p>{text}</p>
+          </div>
+        </div>
+        <Header1 right content={year?.toString()} />
+      </div>
+    );
+  }
 );
 
 export { SpeakingBoble };
