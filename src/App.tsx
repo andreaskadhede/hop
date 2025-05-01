e7852b14379369404a82e29c76e291135ea99f69
Andrea Skadhede <andreaskadhede@icloud.com>
2025-05-01 18:28:01 +0200

    scroll to top added

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,23 @@ import { HomePage } from "./pages/homePage/homePage.js";
 import { AboutOriginalityPage } from "./pages/aboutOriginalityPage/aboutOriginalityPage.js";
 import { AboutUNIwisePage } from "./pages/aboutUNIwisePage/aboutUNIwisePage.js";
 import { AboutWISEflowPage } from "./pages/aboutWISEflowPage/aboutWISEflowPage.js";
+import ScrollToTop from "./components/small/ScrolToTop.js";
 
 function App() {
-   return (
-      <>
-         <Header />
-         <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route
-               path="/AboutOriginality"
-               element={<AboutOriginalityPage />}
-            />
-            <Route path="/AboutUNIwise" element={<AboutUNIwisePage />} />
-            <Route path="/AboutWISEflow" element={<AboutWISEflowPage />} />
-            <Route path="*" element={<Navigate to="/" />} />
-         </Routes>
-         <Footer />
-      </>
-   );
+  return (
+    <>
+      <Header />
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/AboutOriginality" element={<AboutOriginalityPage />} />
+        <Route path="/AboutUNIwise" element={<AboutUNIwisePage />} />
+        <Route path="/AboutWISEflow" element={<AboutWISEflowPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
