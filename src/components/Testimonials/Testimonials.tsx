e3a8e3bcf2dacf5398a3f66d74a8c04936860b10
Andrea Skadhede <andreaskadhede@icloud.com>
2025-05-01 18:29:02 +0200

    left aligned button added

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -12,78 +12,78 @@ import testimonial3 from "../../assets/images/testimonial-3.svg";
 
 // Type definition for a single testimonial
 type Testimonial = {
-   header2Text: string;
-   imageSrc: string;
-   text: string;
+  header2Text: string;
+  imageSrc: string;
+  text: string;
 };
 
 // You can still define your default testimonials data here or elsewhere
 const testimonialData: Testimonial[] = [
-   {
-      header2Text:
-         "Integrating with an LMS and maintaining academic integrity through WISEflow: the Universidade Aberta story",
-      text: "We sat down with Bruno Tavares, Chief Information Security Officer, to discuss the Universidade Aberta’s use of the WISEflow API, integrating with Moodle and how the platform has aided the institution in maintaining academic integrity.",
-      imageSrc: testimonial1,
-   },
-   {
-      header2Text:
-         "From onboarding to innovation: the University of Portsmouth story",
-      text: "When the University of Portsmouth started using WISEflow the initial focus was nly not on using reflective ePortfolios. However, piloting led to a successful onboarding phase, and before they knew it the staff at the University of Portsmouth was pushing the boundaries of digital assessment. Sit back and enjoy our first ever video case study…",
-      imageSrc: testimonial2,
-   },
-   {
-      header2Text:
-         "Reducing the administrative burden, ensuring flexibility and providing authenticity",
-      text: "The Arctic University of Norway (UiT) is a research university that contributes to knowledge-based development at the regional, national and international level. This case study follows a conversation with Maria Mykelstad, Administrative Manager at UiT.   ",
-      imageSrc: testimonial3,
-   },
+  {
+    header2Text:
+      "Integrating with an LMS and maintaining academic integrity through WISEflow",
+    text: "We sat down with Bruno Tavares, Chief Information Security Officer, to discuss the Universidade Aberta’s use of the WISEflow API, integrating with Moodle and how the platform has aided the institution in maintaining academic integrity.",
+    imageSrc: testimonial1,
+  },
+  {
+    header2Text:
+      "From onboarding to innovation: the University of Portsmouth story",
+    text: "When the University of Portsmouth started using WISEflow the initial focus was nly not on using reflective ePortfolios. However, piloting led to a successful onboarding phase, and before they knew it the staff at the University of Portsmouth was pushing the boundaries of digital assessment. Sit back and enjoy our first ever video case study…",
+    imageSrc: testimonial2,
+  },
+  {
+    header2Text:
+      "Reducing the administrative burden, ensuring flexibility and providing authenticity",
+    text: "The Arctic University of Norway (UiT) is a research university that contributes to knowledge-based development at the regional, national and international level. This case study follows a conversation with Maria Mykelstad, Administrative Manager at UiT.   ",
+    imageSrc: testimonial3,
+  },
 ];
 
 const Testimonials = forwardRef<HTMLDivElement>(({}, ref) => {
-   const [current, setCurrent] = useState(0);
+  const [current, setCurrent] = useState(0);
 
-   useEffect(() => {
-      const interval = setInterval(() => {
-         setCurrent((prev) => (prev + 1) % testimonialData.length);
-      }, 5000);
-      return () => clearInterval(interval);
-   }, [testimonialData.length, 5000]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((prev) => (prev + 1) % testimonialData.length);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [testimonialData.length, 5000]);
 
-   const currentTestimonial = testimonialData[current];
+  const currentTestimonial = testimonialData[current];
 
-   return (
-      <div className={classNames(styles.testimonialContainer)} ref={ref}>
-         <div className={classNames(styles.testimonial)}>
-            <Header1 content="Testimonials" white center />
-            <div className={classNames(styles.textAndImage)}>
-               <div className={classNames(styles.textblock)}>
-                  <div className={classNames(styles.text)}>
-                     <Header2 content={currentTestimonial.header2Text} white />
-                     <p>{currentTestimonial.text}</p>
-                  </div>
-                  <Button content="Read more testimonials" inverted />
-               </div>
-               <div
-                  className={classNames(styles.image)}
-                  style={{
-                     backgroundImage: `url(${currentTestimonial.imageSrc})`,
-                  }}
-               />
+  return (
+    <div className={classNames(styles.testimonialContainer)} ref={ref}>
+      <div className={classNames(styles.testimonial)}>
+        <Header1 content="Testimonials" white center />
+        <div className={classNames(styles.textAndImage)}>
+          <div className={classNames(styles.textblock)}>
+            <div className={classNames(styles.text)}>
+              <Header2 content={currentTestimonial.header2Text} white />
+              <p>{currentTestimonial.text}</p>
             </div>
-         </div>
-         {/* Indicators */}
-         <div className={classNames(styles.indicators)}>
-            {testimonialData.map((_, index) => (
-               <div
-                  key={index}
-                  className={classNames(styles.indicator, {
-                     [styles.active]: index === current,
-                  })}
-               />
-            ))}
-         </div>
+            <Button left content="Read more testimonials" inverted />
+          </div>
+          <div
+            className={classNames(styles.image)}
+            style={{
+              backgroundImage: `url(${currentTestimonial.imageSrc})`,
+            }}
+          />
+        </div>
       </div>
-   );
+      {/* Indicators */}
+      <div className={classNames(styles.indicators)}>
+        {testimonialData.map((_, index) => (
+          <div
+            key={index}
+            className={classNames(styles.indicator, {
+              [styles.active]: index === current,
+            })}
+          />
+        ))}
+      </div>
+    </div>
+  );
 });
 
 export { Testimonials };
--- src/components/WiseflowAndOriginality/WiseflowAndOriginality.tsx
+++ src/components/WiseflowAndOriginality/WiseflowAndOriginality.tsx
@@ -7,34 +7,36 @@ import { Button } from "../small/Button/Button";
 
 import WiseflowAndOriginality from "../../assets/images/wiseflow+originality.svg";
 const WISEflowAndOriginality = forwardRef<HTMLDivElement>(({}, ref) => {
-   return (
-      <div className={classNames(styles.wiseflowAndOriginality)} ref={ref}>
-         <div className={classNames(styles.left)}>
-            <div className={classNames(styles.text)}>
-               <Header1 white content="WISEflow and Originality"></Header1>
-               <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac
-                  massa turpis. Suspendisse potenti. Praesent hendrerit neque
-                  sit amet nulla accumsan rutrum. Quisque eget neque non arcu
-                  faucibus consequat ac nec est. In sed purus dui. Nullam at
-                  ante scelerisque, semper arcu eget, pellentesque leo. In
-                  viverra aliquam mattis. Proin sodales maximus felis, sit amet
-                  placerat dolor vulputate id. Praesent ac maximus sem.
-                  Curabitur nibh metus, posuere et blandit at, tincidunt id dui.
-               </p>
-            </div>
-            <Button
-               inverted
-               content="Read more about WISEflow"
-               path="/AboutWISEflow"></Button>
-         </div>
-         <img
-            src={WiseflowAndOriginality}
-            alt="The circle of WISEflow and Originality"
-            className={classNames(styles.image)}
-         />
+  return (
+    <div className={classNames(styles.wiseflowAndOriginality)} ref={ref}>
+      <div className={classNames(styles.left)}>
+        <div className={classNames(styles.text)}>
+          <Header1 white content="WISEflow and Originality"></Header1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac massa
+            turpis. Suspendisse potenti. Praesent hendrerit neque sit amet nulla
+            accumsan rutrum. Quisque eget neque non arcu faucibus consequat ac
+            nec est. In sed purus dui. Nullam at ante scelerisque, semper arcu
+            eget, pellentesque leo. In viverra aliquam mattis. Proin sodales
+            maximus felis, sit amet placerat dolor vulputate id. Praesent ac
+            maximus sem. Curabitur nibh metus, posuere et blandit at, tincidunt
+            id dui.
+          </p>
+        </div>
+        <Button
+          left
+          inverted
+          content="Read more about WISEflow"
+          path="/AboutWISEflow"
+        ></Button>
       </div>
-   );
+      <img
+        src={WiseflowAndOriginality}
+        alt="The circle of WISEflow and Originality"
+        className={classNames(styles.image)}
+      />
+    </div>
+  );
 });
 
 export { WISEflowAndOriginality };
