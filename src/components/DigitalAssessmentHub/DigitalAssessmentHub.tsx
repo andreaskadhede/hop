f9adc6f6eb08bdfd0c94c8c0c76f84740fd386de
Andrea Skadhede <andreaskadhede@icloud.com>
2025-05-06 15:04:24 +0200

    small changes

--- src/components/DigitalAssessmentHub/DigitalAssessmentHub.tsx
+++ src/components/DigitalAssessmentHub/DigitalAssessmentHub.tsx
@@ -15,7 +15,7 @@ function DigitalAssessmentHub() {
             There are many reasons for transitioning to a digital assessment
             platform at your institution…
           </p>
-          <Button content="learn more" />
+          <Button content="Learn more" />
         </div>
         <div className={classNames(styles.hub)}>
           <Header2 content="The change management process" />
@@ -24,7 +24,7 @@ function DigitalAssessmentHub() {
             and explore why effective change management is so important to the
             success of initiatives…
           </p>
-          <Button content="learn more" />
+          <Button content="Learn more" />
         </div>
         <div className={classNames(styles.hub)}>
           <Header2 content="Student satisfaction and digital assessment" />
@@ -33,7 +33,7 @@ function DigitalAssessmentHub() {
             online learning approach, and digital assessment platforms can
             increase student satisfaction in a number of ways…
           </p>
-          <Button content="learn more" />
+          <Button content="Learn more" />
         </div>
         <div className={classNames(styles.hub)}>
           <Header2 content="Easing the administrative burden" />
@@ -41,7 +41,7 @@ function DigitalAssessmentHub() {
             How does transitioning to a digital assessment platform lighten the
             administrative load for university staff?
           </p>
-          <Button content="learn more" />
+          <Button content="Learn more" />
         </div>
         <div className={classNames(styles.hub)}>
           <Header2 content="Data privacy and digital assessment" />
@@ -50,7 +50,7 @@ function DigitalAssessmentHub() {
             promote and ensure data privacy to guarantee the protection of staff
             and students alike…
           </p>
-          <Button content="learn more" />
+          <Button content="Learn more" />
         </div>
         <div className={classNames(styles.hub)}>
           <Header2 content="Designing effective digital assessment" />
@@ -59,7 +59,7 @@ function DigitalAssessmentHub() {
             best practices that we’ve seen used by institutions we support,
             here's a rundown of considerations to keep in mind…
           </p>
-          <Button content="learn more" />
+          <Button content="Learn more" />
         </div>
       </div>
     </div>
